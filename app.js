487ac3f94abbe21a47dba3a02f3faadc4ab7c199
Afuza <thegannoz@gmail.com>
2019-10-29 19:22:04 +0700

    Added landing page
    Along with landing, made header and footer files

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ var express = require("express"),
   app = express(),
   bodyParser = require("body-parser");
 
+app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
-  res.send("HELLLOOOO");
+  res.render("landing");
 });
 
 app.listen(3000, function() {
