42e8ea7839b6b37a34187dd4effd5353703d96de
Afuza <thegannoz@gmail.com>
2019-11-02 15:27:11 +0700

    Connected insalts with mongodb database
    Added salts collection to the database

--- routes/salts.js
+++ routes/salts.js
@@ -7,8 +7,32 @@ router.get("/", function(req, res) {
   res.render("salts/");
 });
 
+// NEW
 router.get("/new", function(req, res) {
   res.render("salts/new");
+});
+
+// CREATE
+router.post("/", function(req, res) {
+  var name = req.body.name;
+  var slogan = req.body.slogan;
+  var description = req.body.description;
+
+  var newSalt = {
+    name: name,
+    slogan: slogan,
+    description: description
+  };
+
+  console.log("test");
+  Salt.create(newSalt, function(err, createdSalt) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      console.log(createdSalt);
+      res.redirect("/s");
+    }
+  });
 });
 
 module.exports = router;
