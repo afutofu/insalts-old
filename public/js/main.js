3355e233258cebf837137f05a220fa4c28a8a966
Afuza <theafuza@gmail.com>
2019-12-02 17:17:44 +0700

    Implemented downvote with AJAX
    Does not save user to upvoted posts

--- public/js/main.js
+++ public/js/main.js
@@ -26,25 +26,39 @@ $(document).ready(function() {
   function upvotePost(upvotedPost, upvoteIcon) {
     var postId = upvotedPost.attr("data-postId");
     var currentVote = parseInt(upvotedPost.attr("data-votes"));
-    var updatedVote = currentVote + 1;
-    var updatedData = { vote: updatedVote };
 
     var voteSpan = $(upvotedPost.children()[1]);
-
     var url = "/api/insalts/" + postId;
 
-    $.ajax({
-      method: "PUT",
-      url: url,
-      data: updatedData
-    })
-      .then(function(updatedPost) {
-        upvoteIcon.toggleClass("i-vote-voted");
-        voteSpan.text(updatedVote);
+    if (!upvoteIcon.hasClass("i-vote-voted")) {
+      var updatedData = { vote: currentVote + 1 };
+      $.ajax({
+        method: "PUT",
+        url: url,
+        data: updatedData
       })
-      .catch(function(err) {
-        console.log(err);
-      });
+        .then(function(updatedPost) {
+          upvoteIcon.toggleClass("i-vote-voted");
+          voteSpan.text(currentVote + 1);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    } else if (upvoteIcon.hasClass("i-vote-voted")) {
+      var updatedData = { vote: currentVote };
+      $.ajax({
+        method: "PUT",
+        url: url,
+        data: updatedData
+      })
+        .then(function(updatedPost) {
+          upvoteIcon.toggleClass("i-vote-voted");
+          voteSpan.text(currentVote);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    }
   }
 
   downvote.on("mouseenter", function() {
@@ -54,6 +68,50 @@ $(document).ready(function() {
   downvote.on("mouseleave", function() {
     postLinks.unbind("click", disableLink);
   });
+
+  // Downvote
+  downvote.on("click", function() {
+    downvotePost($(this).parent(), $(this));
+  });
+
+  function downvotePost(downvotedPost, downvoteIcon) {
+    var postId = downvotedPost.attr("data-postId");
+    var currentVote = parseInt(downvotedPost.attr("data-votes"));
+
+    var voteSpan = $(downvotedPost.children()[1]);
+
+    var url = "/api/insalts/" + postId;
+
+    if (!downvoteIcon.hasClass("i-vote-voted")) {
+      var updatedData = { vote: currentVote - 1 };
+      $.ajax({
+        method: "PUT",
+        url: url,
+        data: updatedData
+      })
+        .then(function(updatedPost) {
+          downvoteIcon.toggleClass("i-vote-voted");
+          voteSpan.text(currentVote - 1);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    } else if (downvoteIcon.hasClass("i-vote-voted")) {
+      var updatedData = { vote: currentVote };
+      $.ajax({
+        method: "PUT",
+        url: url,
+        data: updatedData
+      })
+        .then(function(updatedPost) {
+          downvoteIcon.toggleClass("i-vote-voted");
+          voteSpan.text(currentVote);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    }
+  }
 
   // JOINED BUTTON HOVER TO LEAVE BUTTON
   var joinedBtn = $("#btn-joined");
